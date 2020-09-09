0bd1b2ce6a8217bcb660f4709547a40d5f4c7b37
Nicholas Trunov <anteycu@gmail.com>
2020-09-09 22:46:19 +0300

    done build, ready for deploy

---
+++ build/precache-manifest.91c43b66c05cc01ba6cbdfae6c9d4109.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7459fa5d3408ca0683b55ce745c266",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "6011a844f06721218020",
+    "url": "/goit-react-hw-02-feedback/static/js/2.27b96eb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.27b96eb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a3e885f49421c5457fb",
+    "url": "/goit-react-hw-02-feedback/static/js/main.47e7fffe.chunk.js"
+  },
+  {
+    "revision": "c7f5449263339e63b0ce",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.3577110f.js"
+  }
+]);
